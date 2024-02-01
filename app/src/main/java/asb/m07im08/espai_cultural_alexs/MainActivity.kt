c5840b4872ded09dd4eeda531242d229da1451ed
ASBcep <asaboritb2324@politecnics.barcelona>
2024-02-01 17:05:35 +0100

    Initial commit

---
+++ app/src/main/java/asb/m07im08/espai_cultural_alexs/MainActivity.kt
@@ -0,0 +1,11 @@
+package asb.m07im08.espai_cultural_alexs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
