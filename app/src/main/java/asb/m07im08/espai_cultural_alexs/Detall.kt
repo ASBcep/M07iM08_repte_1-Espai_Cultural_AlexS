3b88c0ccaff2e03f1bc13394b8f6998cefc31438
ASBcep <asaboritb2324@politecnics.barcelona>
2024-02-26 20:28:25 +0100

    Prototip:  detalls fet, creant gestó esdeveniments

--- app/src/main/java/asb/m07im08/espai_cultural_alexs/Detall.kt
+++ app/src/main/java/asb/m07im08/espai_cultural_alexs/Detall.kt
@@ -1,16 +1,38 @@
 package asb.m07im08.espai_cultural_alexs
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 
 class Detall : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detall)
 
-        val txtVwDescripcio: TextView = findViewById(R.id.TxtVwDescripcio)
-        //permetre scroll en el textview
-        txtVwDescripcio.movementMethod = android.text.method.ScrollingMovementMethod.getInstance()
-    }
+        setContentView(R.layout.activity_detall)
+        val lvDetalls = findViewById<ListView>(R.id.lvDetalls)
+
+        val headerView = layoutInflater.inflate(R.layout.textview_layout, null, false)
+        val TxtVwPersonalitzat = headerView.findViewById<TextView>(R.id.TxtVwPersonalitzat)
+        TxtVwPersonalitzat.text = getString(R.string.descripcio_demo)
+
+        // Afegir l'encapçalament abans de configurar l'adaptador
+        lvDetalls.addHeaderView(headerView)
+
+        val llistaDetalls = listOf("Element 1", "Element 2", "Element 3", "Element 4")
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, llistaDetalls)
+        lvDetalls.adapter = adaptador
+
+        val btnModificar = findViewById<Button>(R.id.btnModificar)
+        btnModificar.setOnClickListener {
+            val intent = Intent(this, Gestio_Esdeveniment::class.java)
+            startActivity(intent)
+        }
+   }
 }
--- app/src/main/java/asb/m07im08/espai_cultural_alexs/Gestio_Esdeveniment.kt
+++ app/src/main/java/asb/m07im08/espai_cultural_alexs/Gestio_Esdeveniment.kt
@@ -1,11 +1,31 @@
 package asb.m07im08.espai_cultural_alexs
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class Gestio_Esdeveniment : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_gestio_esdeveniment)
+        val lvDetalls = findViewById<ListView>(R.id.lvDetalls)
+
+        val headerView = layoutInflater.inflate(R.layout.textview_layout, null, false)
+        val TxtVwPersonalitzat = headerView.findViewById<TextView>(R.id.TxtVwPersonalitzat)
+        TxtVwPersonalitzat.text = getString(R.string.descripcio_demo)
+
+        // Afegir l'encapçalament abans de configurar l'adaptador
+        lvDetalls.addHeaderView(headerView)
+
+        val llistaDetalls = listOf("Element 1", "Element 2", "Element 3", "Element 4")
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, llistaDetalls)
+        lvDetalls.adapter = adaptador
+
     }
 }
